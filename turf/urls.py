086766aa78589d32fe1fb9d77611f6cc226b9396
onerealprince <pakanya78@gmail.com>
2025-08-11 18:07:47 +0000

    Update urls.py

--- turf/urls.py
+++ turf/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TurfSpotViewSet
+from .views import (
+    TurfSpotViewSet,
+    PitchTypeViewSet,
+    GameTimeViewSet,
+    PurposeViewSet,
+    FacilityViewSet
+)
 
 router = DefaultRouter()
-router.register(r'turfs', TurfSpotViewSet, basename='turf')
+router.register(r'turfspots', TurfSpotViewSet)
+router.register(r'pitchtypes', PitchTypeViewSet)
+router.register(r'gametimes', GameTimeViewSet)
+router.register(r'purposes', PurposeViewSet)
+router.register(r'facilities', FacilityViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
